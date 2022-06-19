dbcf046429b9276495ba2ef2eb463b979f5c7375
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-06-19 01:34:13 -0300

    + repositório de Venda implementado

--- src/gateaway/repositories/implementations/VendaRepository.ts
+++ src/gateaway/repositories/implementations/VendaRepository.ts
@@ -4,9 +4,20 @@ import { IVendaRepository } from "../interfaces/IVendaRepository";
 
 class VendaRepository implements IVendaRepository {
 
+
+    async getVendasEmAberto(): Promise<IVenda[]> {
+        try{
+            let vendasEmAberto = await Venda.find({ baixa:true }).exec().then(res => res);
+            return vendasEmAberto;
+        } catch(err) {
+            console.log(err);
+            return null;
+        }
+    }
+
     async getVendasByClienteNoDia(clienteId: Number, diaHoje:Date): Promise<IVenda[]> {
         try{
-            let vendasDoClienteNoDia = await Venda.find({__id:clienteId, dataVenda:diaHoje }).exec().then(res=> res);
+            let vendasDoClienteNoDia = await Venda.find({_id:clienteId, dataVenda:diaHoje }).exec().then(res => res);
             return vendasDoClienteNoDia;  
         } catch (err) {
             console.log(err);
@@ -20,6 +31,27 @@ class VendaRepository implements IVendaRepository {
     }
 
 
+    async updateValorPag(vendaId: Number, valorAcrescentado: Number): Promise<IVenda> {
+        
+        let options = { returnDocuments:'after' };
+        let incrementUpdate = {$inc:{'vendas.valorPago': valorAcrescentado}};
+        
+        try {
+            let vendaAtt = await Venda.findOneAndUpdate({_id: vendaId}, incrementUpdate, options).exec().then(res => res);
+            return vendaAtt;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
+
+    async updateBaixa(vendaId: Number): Promise<void> {
+        try {
+            await Venda.updateOne({ _id:vendaId }, { baixa:true });
+        } catch (err) {
+            console.log(err);
+        }
+    }
     
 }
 
