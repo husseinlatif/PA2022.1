d3d1ca40c2504ee4d0dc773925dbb9390dd001e2
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-06-19 12:22:10 -0300

    + repositorio de relatório de cobrança finalizado

--- src/gateaway/repositories/interfaces/IRelFornecedorRepository.ts
+++ src/gateaway/repositories/interfaces/IRelFornecedorRepository.ts
@@ -1,7 +1,7 @@
 import { IRel_Fornecedor } from "../../../domain/interfaces/IRel_Fornecedor";
 
 export interface IRelFornecedorRepository {
-    getRelByDay(wantedDate: Date) : Promise<IRel_Fornecedor>;
-    addRel(object: IRel_Fornecedor) : Promise<void>;
-    deleteRelByDay(wantedDate: Date) : Promise <IRel_Fornecedor>;
+    getRelByFornecedorNoDia(fornecedorId: Number, dia: Date) : Promise<IRel_Fornecedor>;
+    addRel(object: IRel_Fornecedor) : void;
+    //deleteRelsByDay(wantedDate: Date) : Promise <IRel_Fornecedor[]>;
 }
