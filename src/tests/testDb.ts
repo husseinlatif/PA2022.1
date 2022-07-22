f89ffbc4dbfe2d410ce8387274f1440b2e41cee4
Gabriel Ruas <gabrielamaralruas@gmail.com>
2022-07-21 22:16:04 -0300

    # 300 mudanças de config pra rodar o PRIMEIRO TESTE

--- src/tests/testDb.ts
+++ src/tests/testDb.ts
@@ -1,39 +1,38 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import ClienteJson from '../resources/jsonMocks/clientesData.json';
-import AdminJson from '../resources/jsonMocks/adminData.json';
 
+let mongod;
 
-const fakeData = {
-    clientes:ClienteJson,
-    admins:AdminJson
-}
-
-const mongoTestDb = new MongoMemoryServer();
+const createServer = async () => {
+    mongod = await MongoMemoryServer.create(); 
+}; 
 
 const connect = async () => {
-    const uri = await mongoTestDb.getUri();
+    await createServer();
+    const uri = mongod.getUri();
     await mongoose.connect(uri);
 }
 
 const closeDatabase = async() => {
     await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
-    await mongoTestDb.stop();
+    await mongod.stop();
 }
 
 //necessário para termos uma base limpa para cada teste unitário
-const clearDatabase = async() => {
+/* const clearDatabase = async() => {
     let connection = mongoose.connection;
     for(let key in fakeData) {
         await connection.dropCollection(key.toString());
     }
-}
+} */
 
-const addCollectionData = async(fakeCollectionNames:String[]) => {
+/* const addCollectionData = async(fakeCollectionNames:String | String[]) => {
     const collections = mongoose.connection.collections;
     for(let key in fakeCollectionNames) {
         const collection = collections[key];
         collection.insertMany(fakeData[key]);
     }
-}
+} */
+
+export { connect, closeDatabase }
